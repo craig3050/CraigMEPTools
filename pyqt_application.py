5d2d4c08f59c4f8ec67addbae20caca40c1f8e31
Craig Cuninghame <craig3050@gmail.com>
2019-01-27 21:31:24 +0000

    Works, added website link

--- pyqt_application.py
+++ pyqt_application.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QToolTip, QPushButton, QApplication, QLabel, QGraphicsAnchorLayout, QGridLayout, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QToolTip, QPushButton, QApplication, QLabel, QGraphicsAnchorLayout, QGridLayout, QVBoxLayout, QDesktopWidget
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import Qt
 
@@ -25,7 +25,8 @@ class MainWindow(QWidget):
         grid.addWidget(self.welcome_title, 0, 0, 1, 3)
         # Author Info
         self.author_info = QLabel(self, alignment=Qt.AlignRight | Qt.AlignVCenter)
-        self.author_info.setText('Craig Cuninghame - Cuninghame Design Ltd')
+        self.author_info.setText('Craig Cuninghame -  <a href="http://www.cuninghamedesign.co.uk/">Cuninghame Design Ltd</a>')
+        self.author_info.setOpenExternalLinks(True)
         self.author_info.adjustSize()
         grid.addWidget(self.author_info, 8, 1, 1, 3)
 
@@ -40,11 +41,20 @@ class MainWindow(QWidget):
         grid.setRowStretch(1, 1)
 
         # Setup the overall window
-        self.setGeometry(300, 300, 700, 600)  # Window size and position on screen
+        self.resize(300, 300)  # Window size <<and position on screen .setGeometry(300, 300, 700, 600)>>
+        self.center()
+
+
         self.setFixedSize(550, 400)  # Fix the window size so it does't re-size
         self.setWindowTitle('Craig MEP Toolkit')
         self.setWindowIcon(QIcon('web.png'))
         self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
     def fileRenamerButton(self):
         QToolTip.setFont(QFont('SansSerif', 10))
@@ -84,6 +94,7 @@ class MainWindow(QWidget):
         QToolTip.setFont(QFont('SansSerif', 10))
 
         btn = QPushButton('Exit', self)
+        btn.clicked.connect(QApplication.instance().quit)
         # btn.setToolTip('Get me out of here!')
         btn.resize(400, 50)
         btn.move(75, 290)
