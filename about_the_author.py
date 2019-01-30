6cd1657afe981e7d288a8de43e367c340e211faf
Craig Cuninghame <craig3050@gmail.com>
2019-01-30 21:21:02 +0000

    Added pictures and worked out grid layouts

--- about_the_author.py
+++ about_the_author.py
@@ -1,6 +1,7 @@
 import sys
+import os
 from PyQt5.QtWidgets import QWidget, QToolTip, QPushButton, QApplication, QLabel, QGraphicsAnchorLayout, QGridLayout, QVBoxLayout, QDesktopWidget
-from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtGui import QIcon, QFont, QPixmap
 from PyQt5.QtCore import Qt
 
 
@@ -22,19 +23,17 @@ class AboutTheAuthor(QWidget):
         self.welcome_title = QLabel(self, alignment=Qt.AlignLeft | Qt.AlignVCenter)
         self.welcome_title.setText("<b>About the Author:</b>")
         self.welcome_title.adjustSize()
-        grid.addWidget(self.welcome_title, 0, 0, 1, 3)
+        grid.addWidget(self.welcome_title, 0, 0, 1, 1)
         # Author Info
         self.author_info = QLabel(self, alignment=Qt.AlignRight | Qt.AlignVCenter)
         self.author_info.setText('Craig Cuninghame -  <a href="http://www.cuninghamedesign.co.uk/">Cuninghame Design Ltd</a>')
         self.author_info.setOpenExternalLinks(True)
         self.author_info.adjustSize()
-        grid.addWidget(self.author_info, 3, 1, 1, 3)
+        grid.addWidget(self.author_info, 3, 1, 1, 2)
 
         # Add button links to the functions
-        self.introduction_text = QLabel(self, alignment=Qt.AlignLeft | Qt.AlignVCenter)
+        self.introduction_text = QLabel(self, alignment=Qt.AlignLeft)
         self.introduction_text.setText("Hi, my name is Craig Cuninghame\n"
-                                       "I am an Electrical Engineer by trade\n "
-                                       "and a part time programmer by night\n"
                                        "Lorem ipsum dolor sit amet, mei no case doctus, te vix errem nominati, "
                                        "ut quo scripta alterum placerat. Eum natum assentior at, ex est debet laudem. "
                                        "Sea altera offendit id, quo melius albucius ex. In cum etiam repudiandae, ea "
@@ -46,12 +45,12 @@ class AboutTheAuthor(QWidget):
                                        "duo ex possit eirmod noluisse.")
         self.introduction_text.adjustSize()
         self.introduction_text.setWordWrap(True)
-        grid.addWidget(self.introduction_text, 1, 0, 1, 3)
+        grid.addWidget(self.introduction_text, 1, 0, 1, 2)
 
-        # pic = QtGui.QLabel(window)
-        # pic.setGeometry(10, 10, 400, 100)
-        # # use full ABSOLUTE path to the image, not relative
-        # pic.setPixmap(QtGui.QPixmap(os.getcwd() + "/logo.png"))
+        self.pic = QLabel(self, alignment=Qt.AlignTop)
+        grid.addWidget(self.pic, 1, 2)
+        # use full ABSOLUTE path to the image, not relative
+        self.pic.setPixmap(QPixmap(os.getcwd() + "/Craig_Photo.jpg"))
 
         # Universal parameter for text grids
         grid.setRowStretch(1, 1)
@@ -64,7 +63,7 @@ class AboutTheAuthor(QWidget):
         self.setFixedSize(1200, 800)  # Fix the window size so it does't re-size
         self.setWindowTitle('Craig\'s MEP Toolkit')
         self.setWindowIcon(QIcon('web.png'))
-        # self.show()
+
 
     def center(self):
         qr = self.frameGeometry()
