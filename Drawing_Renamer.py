2209e01c0fad7a459c7f5fb5cc03e8e40d4a54df
Craig Cuninghame <craig3050@users.noreply.github.com>
2020-09-04 11:18:04 +0100

    File renamer successfully ported into new GUI
    Still to do:
    Hyperlinks on front page
    Standards search programme
    File path export programme
    Picture compression

---
+++ Drawing_Renamer.py
@@ -0,0 +1,129 @@
+import os
+import pdfplumber
+import re
+from openpyxl import Workbook
+from openpyxl import load_workbook
+import time
+
+
+class Drawing_Renamer_Tools:
+
+    #defining constructor
+    def __init__(self, filePath, main_file_list):
+        self.file_path = filePath
+        self.main_file_list = main_file_list
+
+
+    def return_list_of_files(self):
+        for file_name in os.listdir(self.file_path):
+            self.main_file_list[file_name] = file_name
+        return self.main_file_list
+
+
+    def enter_beginningtext(self, beginning_text):
+        for item in self.main_file_list:
+            current_text = self.main_file_list[item]
+            update_text = f'{beginning_text}{current_text}'
+            self.main_file_list[item] = update_text
+        return self.main_file_list
+
+
+    def enter_endtext(self, end_text):
+        for item in self.main_file_list:
+            file_extension = item.split(".")[-1]
+            current_text = self.main_file_list[item].split(".")[0]
+            update_text = f'{current_text}{end_text}.{file_extension}'
+            self.main_file_list[item] = update_text
+        return self.main_file_list
+
+
+    def enter_replace_text(self, replace_text_before, replace_text_after):
+        for item in self.main_file_list:
+            current_text = self.main_file_list[item]
+            update_text = current_text.replace(replace_text_before, replace_text_after)
+            self.main_file_list[item] = update_text
+        return self.main_file_list
+
+
+    def enter_titleblock_search(self, full_file_path, sample_drawing_number):
+        # rename the file based on what is in the titleblock
+        # Open the file and extract text with PyPDF2
+        with pdfplumber.open(full_file_path) as pdf:
+            full_text = ""
+            pages = pdf.pages
+            for i, pg in enumerate(pages):
+                text = pages[i].extract_text()
+                full_text += text
+            # Convert the sample filename into regex search and search the extracted text
+            output_text = ""
+            regex_variable = "."
+            for char in sample_drawing_number:
+                if char == "-":
+                    output_text += "-"
+                elif char == "_":
+                    output_text += "_"
+                elif char == "(":
+                    output_text += "\("
+                elif char == ")":
+                    output_text += "\)"
+                else:
+                    output_text += regex_variable
+            regex_results = re.findall(output_text, full_text)
+            number_of_regex_groups = len(regex_results) - 1
+            best_guess_drawing_number = regex_results[number_of_regex_groups]
+        return best_guess_drawing_number
+
+
+    def rename_file(self, old_file_name, new_file_name):
+        os.rename(old_file_name, new_file_name)
+
+
+
+    def excel_list_export(self):
+        drawing_list = Workbook()
+        worksheet_1 = drawing_list.active
+        drawing_list_file_path = self.file_path + "/" + "Drawing_List.xlsx"
+        drawing_list.save(drawing_list_file_path)
+        row_number = 1
+        for key, values in self.main_file_list.items():
+            file_name_no_extension = ""
+            for char in key:
+                if char != ".":
+                    file_name_no_extension += char
+                else:
+                    break
+            file_extension = key.split(".")[-1]
+            worksheet_1.cell(row=row_number, column=1).value = file_name_no_extension
+            worksheet_1.cell(row=row_number, column=2).value = file_extension
+            worksheet_1.cell(row=row_number, column=3).value = file_name_no_extension
+            row_number += 1
+        drawing_list.save(drawing_list_file_path)
+
+
+
+    def excel_list_import(self):
+        drawing_list = Workbook()
+        worksheet_1 = drawing_list.active
+        drawing_list_file_path = self.file_path + "/" + "Drawing_List.xlsx"
+        drawing_list = load_workbook(drawing_list_file_path)
+        worksheet_1 = drawing_list.active
+        row_number = worksheet_1.max_row
+        row_number += 1  # to adjust the count as it starts from 0
+        for i in range(1, row_number):
+            old_file_name = worksheet_1.cell(row=i, column=1).value
+            file_extension = worksheet_1.cell(row=i, column=2).value
+            new_file_name = worksheet_1.cell(row=i, column=3).value
+            old_file_name_plus_extension = old_file_name + "." + file_extension
+            new_file_name_plus_extension = new_file_name + "." + file_extension
+            self.main_file_list[old_file_name_plus_extension] = new_file_name_plus_extension
+        return self.main_file_list
+
+
+
+    def abouttheauthor(self):
+        self.AuthorWindow = QtWidgets.QMainWindow()
+        self.ui = About_The_Author()
+        self.ui.setupUi(AuthorWindow)
+        self.AuthorWindow.show()
+        # open_author_window = About_The_Author()
+        # open_author_window.show()
--- MainWindow.py
+++ MainWindow.py
@@ -1,8 +1,15 @@
 import sys
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QMainWindow
+from PyQt5.QtGui import QIcon
 
 from MainUI import Ui_MainWindow
+
+from Drawing_Renamer import Drawing_Renamer_Tools
+
+##Global Variables##
+main_file_list = {}
+file_path = ""
 
 class MainWindow:
     def __init__(self):
@@ -40,6 +47,136 @@ class MainWindow:
         self.ui.stackedWidget.setCurrentWidget(self.ui.page_drawing_renamer)
     def show_page_standardssearch(self):
         self.ui.stackedWidget.setCurrentWidget(self.ui.page_standards_search)
+
+
+    def enter_path(self):
+        global file_path
+        global main_file_list
+        main_file_list.clear()
+        file_path = QFileDialog.getExistingDirectory(None, "Open a folder", "C:\\", QFileDialog.ShowDirsOnly)
+        file_details = Drawing_Renamer_Tools(file_path, main_file_list)
+        main_file_list = file_details.return_list_of_files()
+        self.ui.label_pathname.setText(file_path)
+        self.ui.listWidget.clear()
+        for file_name in main_file_list:
+            self.ui.listWidget.addItem(file_name)
+        return
+
+    def enter_beginningtext(self):
+        global file_path
+        global main_file_list
+        file_details = Drawing_Renamer_Tools(file_path, main_file_list)
+        beginning_text = self.ui.lineEdit_enter_beginningtext.text()
+        self.ui.listWidget.clear()
+        main_file_list = file_details.enter_beginningtext(beginning_text)
+        for key, values in main_file_list.items():
+            text_to_print = f'{key} =====> {values}'
+            self.ui.listWidget.addItem(text_to_print)
+        return
+
+    def enter_endtext(self):
+        global file_path
+        global main_file_list
+        file_details = Drawing_Renamer_Tools(file_path, main_file_list)
+        end_text = self.ui.lineEdit_enter_endtext.text()
+        self.ui.listWidget.clear()
+        main_file_list = file_details.enter_endtext(end_text)
+        for key, values in main_file_list.items():
+            text_to_print = f'{key} =====> {values}'
+            self.ui.listWidget.addItem(text_to_print)
+        return
+
+    def enter_replace_text(self):
+        global file_path
+        global main_file_list
+        file_details = Drawing_Renamer_Tools(file_path, main_file_list)
+        replace_text_before = self.ui.lineEdit_enter_replace_text_before.text()
+        replace_text_after = self.ui.lineEdit_enter_replace_text_after.text()
+        self.ui.listWidget.clear()
+        main_file_list = file_details.enter_replace_text(replace_text_before, replace_text_after)
+        for key, values in main_file_list.items():
+            text_to_print = f'{key} =====> {values}'
+            self.ui.listWidget.addItem(text_to_print)
+        return
+
+    def enter_titleblock_search(self):
+        global file_path
+        global main_file_list
+        file_details = Drawing_Renamer_Tools(file_path, main_file_list)
+        sample_drawing_number = self.ui.lineEdit_enter_titleblock_search.text()
+        self.ui.listWidget.clear()
+        self.ui.listWidget.addItem("Please wait - this may take a while...")
+        self.ui.listWidget.addItem("Do not close window until complete - new drawings will list as they are found\n")
+        for item in main_file_list:
+            try:
+                file_extension = item.split(".")[-1]
+                full_file_path = f'{file_path}/{item}'
+                best_guess_drawing_number = file_details.enter_titleblock_search(full_file_path, sample_drawing_number)
+                main_file_list[item] = f'{best_guess_drawing_number}.{file_extension}'
+                self.ui.listWidget.addItem(best_guess_drawing_number)
+                QtCore.QCoreApplication.processEvents()
+            # If it all goes wrong leave filename as is
+            except Exception as e:
+                main_file_list[item] = item
+                self.ui.listWidget.addItem(e)
+        for key, values in main_file_list.items():
+            text_to_print = f'{key} =====> {values}'
+            self.ui.listWidget.addItem(text_to_print)
+        return
+
+    def rename_file(self):
+        global file_path
+        global main_file_list
+        file_details = Drawing_Renamer_Tools(file_path, main_file_list)
+        self.ui.listWidget.clear()
+        self.ui.listWidget.addItem("Renaming files")
+        temp_dict = {}
+        try:
+            for key, values in main_file_list.items():
+                new_file_name = file_path + "/" + values
+                old_file_name = file_path + "/" + key
+                file_details.rename_file(old_file_name, new_file_name)
+                self.ui.listWidget.addItem("Renaming files")
+                text_to_print = f'{old_file_name} =====> {new_file_name}'
+                self.ui.listWidget.addItem(text_to_print)
+                temp_dict[values] = values
+        except Exception as e:
+            self.ui.listWidget.addItem(e)
+        main_file_list = temp_dict
+        self.ui.listWidget.addItem("Rename Completed Successfully")
+
+    def excel_list_export(self):
+        try:
+            global file_path
+            global main_file_list
+            file_details = Drawing_Renamer_Tools(file_path, main_file_list)
+            self.ui.listWidget.clear()
+            self.ui.listWidget.addItem("Saving files to excel sheet...")
+            file_details.excel_list_export()
+            self.ui.listWidget.clear()
+            self.ui.listWidget.addItem("Excel file called \'Drawing_list\' has been saved in the same folder as the drawings")
+            self.ui.listWidget.addItem("\n\nAdd your new drawing numbers into Column C")
+            self.ui.listWidget.addItem("\n\nOnce complete make sure the file is closed, then press \'2.Import\'")
+        except Exception as e:
+            self.ui.listWidget.clear()
+            self.ui.listWidget.addItem("Something has gone wrong - see exception below:\n\n")
+            self.ui.listWidget.addItem(e)
+
+    def excel_list_import(self):
+        try:
+            global file_path
+            global main_file_list
+            file_details = Drawing_Renamer_Tools(file_path, main_file_list)
+            self.ui.listWidget.clear()
+            main_file_list = file_details.excel_list_import()
+            self.ui.listWidget.addItem("Displaying Values:\n")
+            for key, values in main_file_list.items():
+                text_to_print = f'{key} =====> {values}'
+                self.ui.listWidget.addItem(text_to_print)
+        except Exception as e:
+            self.ui.listWidget.clear()
+            self.ui.listWidget.addItem("Something has gone wrong - see exception below:\n\n")
+            self.ui.listWidget.addItem(e)
 
 
 if __name__ == '__main__':
